aaf38271ef511311f93a6e26fc7ffeadd2e370f6
muhmmed_medhat <muhmmedmedhat0@gmail.com>
2024-01-13 17:03:32 +0200

    add seo

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,42 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface SharedMetaSocial extends Schema.Component {
+  collectionName: 'components_shared_meta_socials';
+  info: {
+    displayName: 'metaSocial';
+  };
+  attributes: {
+    socialNetwork: Attribute.Enumeration<['facebook', 'twitter', 'linkedin']>;
+    title: Attribute.String;
+    description: Attribute.Text;
+    image: Attribute.Media & Attribute.Required;
+  };
+}
+
+export interface SharedSeo extends Schema.Component {
+  collectionName: 'components_shared_seos';
+  info: {
+    displayName: 'seo';
+    icon: 'server';
+  };
+  attributes: {
+    metaTitle: Attribute.String;
+    metaDescription: Attribute.String;
+    metaImage: Attribute.Media;
+    metaSocial: Attribute.Component<'shared.meta-social', true>;
+    keywords: Attribute.String;
+    metaRobots: Attribute.String;
+    structuredData: Attribute.JSON;
+    metaViewport: Attribute.String;
+    canonicalURL: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'shared.meta-social': SharedMetaSocial;
+      'shared.seo': SharedSeo;
+    }
+  }
 }
